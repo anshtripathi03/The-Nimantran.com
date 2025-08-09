4af0837379c3408543c69df6216ee5f7f7c55d48
ayushsingh212 <ayurajput249@gmail.com>
2025-08-10 03:07:46 +0530

    ds

--- NimatranFrontend/vite.config.js
+++ NimatranFrontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'https://localhost:8080',
+        target: 'https://newlive.onrender.com',
         changeOrigin: true,
         secure: false,
       },
